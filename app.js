685cdce09dd48e8fa3bcb82a2ed52fc70e33148a
Arnor Ameti <arnameti@gmail.com>
2022-05-17 11:15:18 +0200

    Finished Project

--- app.js
+++ app.js
@@ -3,3 +3,40 @@ Public API Repo
 https://github.com/public-apis/public-apis
 */
 
+const cat = document.querySelector(".cat");
+const fox = document.querySelector(".fox");
+const dog = document.querySelector(".dog");
+
+const catButton = document.querySelector(".get-cat");
+const foxButton = document.querySelector(".get-fox");
+const dogButton = document.querySelector(".get-dog");
+
+// - get Random Cat Images
+const getRandomCat = () => {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((res) => res.json())
+    .then((data) => {
+      const catImageUrl = data[0].url;
+      cat.innerHTML = `<img src="${catImageUrl}" />`;
+    });
+};
+
+// - get Random Fox Images
+const getRandomFox = async () => {
+  const res = await fetch("https://randomfox.ca/floof/");
+  const data = await res.json();
+  const foxImageUrl = data.image;
+  fox.innerHTML = `<img src="${foxImageUrl}" />`
+};
+
+// - get Random Fox Images
+const getRandomDog = async () => {
+  const res = await fetch("https://random.dog/woof.json");
+  const data = await res.json();
+  const dogImageUrl = data.url;
+  dog.innerHTML = `<img src="${dogImageUrl}" />`;
+}
+
+catButton.addEventListener("click", getRandomCat);
+foxButton.addEventListener("click", getRandomFox);
+dogButton.addEventListener("click", getRandomDog);
